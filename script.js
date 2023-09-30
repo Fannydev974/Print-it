c5687f6d0f105b7d297acabf274b139acde829c8
Fanny Hadji <devfanny91@outlook.com>
2023-09-30 20:00:58 +0200

    ajout logo, et code javascript bullet_point et dynamique

--- script.js
+++ script.js
@@ -15,4 +15,60 @@ const slides = [
         "image": "slide4.png",
         "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
     }
-]
+];
+
+// Variables
+let i = 0;
+const leftEl = document.querySelector(".arrow_left");
+const rightEl = document.querySelector(".arrow_right");
+const dotsContainer = document.querySelector(".dots");
+const img = document.querySelector(".banner-img");
+const txt = document.querySelector("p");
+
+
+// Créez et ajoutez les éléments .dot enfants en fonction du tableau slides
+for (let index = 0; index < slides.length; index++) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    dotsContainer.appendChild(dot);
+}
+
+// Sélectionner à nouveau les éléments (.dot) après les avoirs ajoutés
+const dotsEl = dotsContainer.querySelectorAll(".dot");
+
+//Ajouter la classe dot_selected à la première bullet (index 0)
+dotsEl[0].classList.add("dot_selected");
+
+// Écouteurs d'événements pour les boutons gauche et droite (=> a la place du mot function)
+leftEl.addEventListener("click", () => {
+    i--;
+    if (i === -1) {
+        i = slides.length - 1;
+    }
+    img.src = slides[i].image;
+    txt.innerHTML = slides[i].tagLine;
+    dynamicBullets(i);
+});
+
+rightEl.addEventListener("click", () => {
+    i++;
+    if (i === slides.length) {
+        i = 0;
+    }
+    img.src = slides[i].image;
+    txt.innerHTML = slides[i].tagLine;
+    dynamicBullets(i);
+});
+
+// Fonction pour mettre les bullets dynamiquement blanc/gris
+function dynamicBullets(count) {
+    dotsEl.forEach((dot, index) => {
+        if (index === count) {
+            dot.classList.add("dot_selected");
+        } else {
+            dot.classList.remove("dot_selected");
+        }
+    });
+}
+
+
